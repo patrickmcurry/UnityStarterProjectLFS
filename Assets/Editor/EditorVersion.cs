f724cc81a76e78c16a1bcb84f0c8e54640e4f1c7
Patrick Curry <gh@patrickcurry.com>
2018-05-15 15:45:17 -0500

    Update editor / project version code to only use Unity 5.x style ProjectVersion.txt, and it correctly only writes this file if the project is opened in Unity 4.x.

--- Assets/Editor/EditorVersion.cs
+++ Assets/Editor/EditorVersion.cs
@@ -6,13 +6,10 @@ using System.IO;
 public class EditorVersion {
 	static EditorVersion()
 	{
-		StreamWriter writer = new StreamWriter("Assets/Editor/EditorVersion.txt");
-		writer.WriteLine(Application.unityVersion + "");
+#if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_4_8 || UNITY_4_9
+        StreamWriter writer = new StreamWriter("ProjectSettings/ProjectVersion.txt");
+		writer.WriteLine("m_EditorVersion: " + Application.unityVersion + "\n");
 		writer.Close();
-		
-		// Official Unity 5 style editor version tracking too
-		writer = new StreamWriter("ProjectSettings/ProjectVersion.txt");
-		writer.WriteLine("m_EditorVersion: " + Application.unityVersion);
-		writer.Close();
+#endif
 	}
 }
