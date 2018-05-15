1e81d682baca202d19ab16be1e302389ec930b91
Patrick Curry <gh@patrickcurry.com>
2018-05-15 15:54:59 -0500

    Updating the code that forces IL2CPP when building for iOS when using Unity 5.x or newer.

--- Assets/Editor/Unity5iOSUpdate.cs
+++ Assets/Editor/Unity5iOSUpdate.cs
@@ -6,13 +6,13 @@ using UnityEditor;
 public class Unity5iOSUpdate {
 	static Unity5iOSUpdate()
 	{
-#if UNITY_5
-		// Set the project to build with IL2CPP
+#if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_4_8 || UNITY_4_9)
+		// Set the project to build for iOS with IL2CPP
 		PlayerSettings.SetPropertyInt("ScriptingBackend", 1, BuildTargetGroup.iOS);
 
 		// Set the project to build both 32 and 64-bit for iOS
 		PlayerSettings.SetPropertyInt("Architecture", 2, BuildTargetGroup.iOS);
 #endif
-	}
+    }
 }
 
